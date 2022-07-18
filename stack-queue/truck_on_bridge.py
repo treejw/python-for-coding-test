10a62f352bb719e67c8d4d72c8cbb3e027c35859
treejw <42428487+treejw@users.noreply.github.com>
2022-07-18 17:15:08 +0900

    update

--- stack-queue/truck_on_bridge.py
+++ stack-queue/truck_on_bridge.py
@@ -4,15 +4,17 @@
 from collections import deque
 
 def solution(bridge_length, weight, truck_weights):
-    wait = deque(truck_weights)             
-    on_brdg = deque([0]*bridge_length)   
-    on_brdg_weight = 0
+    wait = deque(truck_weights)             # 대기중인 트럭
+    on_brdg = deque([0]*bridge_length)      # 다리 위 상태  (ex) bridge_length=4 → on_brdg=[0, 0, 0, 0]
+    on_brdg_weight = 0                      # 현재 다리 위에 있는 트럭 무게 총 합
     
-    t = 0 
-    while wait or on_brdg_w > 0:
+    t = 0
+    while wait or (on_brdg_weight > 0):
+        # 다리 위 - 맨 앞 제거
         x = on_brdg.popleft()                             
         on_brdg_weight -= x                      
         
+        # 다리 위 - 맨 뒤 추가
         if wait and (on_brdg_weight + wait[0] <= weight): 
             new = wait.popleft()
             on_brdg.append(new)
