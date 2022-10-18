9763cdfa5303c978fb6ef006e558147a8770069f
treejw <42428487+treejw@users.noreply.github.com>
2022-10-18 18:20:04 +0900

    Create 11722.py

---
+++ baekjoon/11722.py
@@ -0,0 +1,14 @@
+import sys
+input = sys.stdin.readline
+
+def func(N, A):
+    dp = [1]*N
+    for cur in range(1, N):
+        for prev in range(cur):
+            if A[cur] < A[prev] and dp[cur] <= dp[prev]:
+                dp[cur] = dp[prev] + 1
+    return max(dp)
+    
+N = int(input().strip())
+A = list(map(int, input().strip().split()))
+print(func(N, A))    
