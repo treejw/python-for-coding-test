04b0e6fdb2d2198f46d91fbb1333b700e4a0a880
treejw <42428487+treejw@users.noreply.github.com>
2022-11-16 23:08:31 +0900

    Update 4948.py

--- baekjoon/4948.py
+++ baekjoon/4948.py
@@ -1,25 +1,21 @@
-from collections import defaultdict
 import sys
 input = lambda: sys.stdin.readline().strip()
 
-def prime_dp(num):
-    is_prime = defaultdict(lambda: True)
-    is_prime[1] = False
-    n = num*2
-    for i in range(2, int(n**0.5)+1):
+def prime_dp(limit=123456):
+    n = limit*2
+    is_prime = [1]*(n+1)
+    is_prime[1] = 0
+
+    for i in range(3, int(n**0.5)+1, 2):
         if is_prime[i]:
-            for j in range(i*i, n+1, i):
-                is_prime[j] = False
+            for j in range(i*i, n+1, i*2):
+                is_prime[j] = 0
     return is_prime
 
 def solution(n, is_prime):
     if n == 1: return 1
-    ans = 0
     s = n+1 if n%2==1 else n
-    for num in range(s+1, 2*n, 2):
-        if (num not in is_prime) or is_prime[num]:
-            ans += 1
-    return ans
+    return sum(is_prime[s+1:(2*n):2])
 
 is_prime = prime_dp(123456)
 while True:
